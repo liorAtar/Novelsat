ec2c13a9895f4cd341904720a3646add104c7043
lior atar <lioratar@h-MacBook-Pro-sl-lior-2.local>
2022-07-06 11:31:48 +0300

    added todo list delete and completed

--- src/Components/ToDoItem.js
+++ src/Components/ToDoItem.js
@@ -1,25 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import Checkbox from '@mui/material/Checkbox';
 import ListItem from '@mui/material/ListItem';
+import '../Css/ToDoItemStyle.css';
 
-const ToDoItem = () => {
+const ToDoItem = ({ todoItem, deleteItem, handleIsCompletedChanged }) => {
+    
+    const [isChecked, setIsChecked] = useState(todoItem.isCompleted);
+
     return (
-        <ListItem
-            style={{
-                backgroundColor: 'aliceblue',
-            }}
+        <ListItem className="listItem"
             secondaryAction={
                 <div>
-                    <Checkbox />
-                    <IconButton aria-label="delete">
+                    <Checkbox
+                        checked={isChecked}
+                        onChange={(e) => {
+                            handleIsCompletedChanged(e, todoItem);
+                            setIsChecked(e.target.checked);
+                            }
+                        }
+                    />
+                    <IconButton
+                        aria-label="delete"
+                        onClick={() => deleteItem(todoItem)}
+                    >
                         <DeleteIcon />
                     </IconButton>
                 </div>
             }
         >
-            <p style={{ color: 'darkslategray'}}>Clean my room</p>
+            <p className="task-p">{todoItem.task}</p>
         </ListItem>
     );
 }
