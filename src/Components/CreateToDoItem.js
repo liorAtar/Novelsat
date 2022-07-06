8d7966f2bf7f56d6816464486d00fe1a03c40007
lior atar <lioratar@h-MacBook-Pro-sl-lior-2.local>
2022-07-06 11:53:30 +0300

    added create todo

---
+++ src/Components/CreateToDoItem.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import Stack from '@mui/material/Stack';
+
+import '../Css/CreateToDoItemStyle.css';
+
+const CreateToDoItem = ({ todoList, setTodoList }) => {
+
+    const [textValue, setTextValue] = useState("");
+
+    const createNewTodo = () => {
+        const newTodo = {
+            id: (todoList[todoList.length - 1].id + 1),
+            task: textValue,
+            isCompleted: false
+        }
+        
+        setTodoList(prev => [...prev, newTodo]);  
+    }
+
+    return (
+        <div className="create-div">
+            <TextField
+                label="To Do"
+                variant="outlined"
+                className="task-text"
+                onChange={(e) => setTextValue(e.target.value)}
+            />
+            <Button
+                variant="contained"
+                endIcon={<AddIcon />}
+                onClick={createNewTodo}
+            >
+                Create
+            </Button>
+        </div>
+    );
+}
+
+export default CreateToDoItem;
